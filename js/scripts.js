40c6a840b99d77902fd7e5ba720850dc8b99ec14
theutas <63684063+theutas@users.noreply.github.com>
2024-06-11 14:37:04 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -185,6 +185,26 @@ function modeloAparelho(nome = new String, model, img, din, cart, din2, cart2, d
 }
 
 
+// ============= CELULARES ADICIONADOS ============= \\ 
+/*
+
+ - SAMSUNG 
+A01
+
+ - IPHONES 
+IPHONE 6
+IPHONE 6S
+IPHONE 7
+IPHONE 7 PLUS
+
+ - MOTOROLA
+
+ - XIOME
+
+*/
+// ================================================= \\
+
+
 function encontrarModelo(){
 
     var modeloPesquisado = encontrar.value.toLocaleUpperCase()                       
@@ -201,6 +221,8 @@ function encontrarModelo(){
           case 'A1589':
           case 'IPHONE 6':
           case 'IPHONE 6 NORMAL':
+          case 'IP 6':
+          case 'IP6':
               document.getElementById("container-frontalSemAro").style.display = "none";
               document.getElementById("bnt_infor_com_aro").style.display = "none";
               modeloAparelho("Iphone 6 Normal", "A1586", "./imgs/modelos/IP6N.png", 200+",00", 220+",00", 150+",00", 160+",00", 100+",00", 110);
@@ -210,6 +232,8 @@ function encontrarModelo(){
           case 'A1700':
           case 'IPHONE 6S':
           case 'IPHONE 6 S':
+          case '6 S':
+          case '6S':
               document.getElementById("container-frontalSemAro").style.display = "none";
               document.getElementById("bnt_infor_com_aro").style.display = "none";
               modeloAparelho("Iphone 6S", "A1633 - A1688 - A1700", "./imgs/modelos/IP6S.jpg", 200+",00", 220+",00", 150+",00", 160+",00", 100+",00", 110);
@@ -219,6 +243,7 @@ function encontrarModelo(){
           case 'A1779 ':
           case 'IPHONE 7':
           case 'IPHONE 7 NORMAL':
+          case '7 NORMAL':
               document.getElementById("container-frontalSemAro").style.display = "none";
               document.getElementById("bnt_infor_com_aro").style.display = "none";
               modeloAparelho("Iphone 7 Normal", "A1586 - A1778 - A1779", "./imgs/modelos/IP7N.jpg", 200+",00", 220+",00", 150+",00", 160+",00", 100+",00", 110);
@@ -227,6 +252,9 @@ function encontrarModelo(){
           case 'A1784':
           case 'A1785':
           case 'IPHONE 7 PLUS':
+          case '7 PLUS':
+              document.getElementById("container-frontalSemAro").style.display = "none";
+              document.getElementById("bnt_infor_com_aro").style.display = "none";
               modeloAparelho("Iphone 7 PLUS", "A1661 - A1784 - A1785", "./imgs/modelos/IP7P.jpg", 200+",00", 220+",00", 150+",00", 160+",00", 100+",00", 110);
               break; 
               
